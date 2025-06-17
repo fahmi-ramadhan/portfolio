babd2733b38e7b644e82c9c094b7eff52eb0be1d
fahmi-ramadhan <fahmi122004@gmail.com>
2025-06-17 13:50:06 +0700

    feat: footer

---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className="py-6 bg-background border-t border-primary/20">
+      <p className="text-muted-foreground text-center">
+        © {new Date().getFullYear()} Fahmi Ramadhan. All rights reserved.
+      </p>
+    </footer>
+  )
+}
